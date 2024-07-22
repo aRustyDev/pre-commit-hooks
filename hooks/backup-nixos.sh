2b7a14d3dadb6490324afdfd9a5b6b1b68573f96
aRustyDev <36318507+aRustyDev@users.noreply.github.com>
2024-07-22 01:02:35 -0400

    v0.1.3 (#7)
    * added 'backup-nixos-config' hook
    
    * added .licrc
    
    * chmod +x hooks/backup-nixos.sh
    
    * added check-shebang-scripts-are-executable
    
    * changed shebang to /usr/bin/env bash
    
    * explicitly set GIT_DIR w/ 'git rev-parse --show-toplevel'
    
    * added comments
    
    * changed to pull files not dir
    
    * catching up (#6)
    
    * Dev (#3)
    
    * added 'backup-nixos-config' hook
    
    * added .licrc
    
    * chmod +x hooks/backup-nixos.sh
    
    * added check-shebang-scripts-are-executable
    
    * switch to `/usr/bin/env bash` (#4)
    
    * added 'backup-nixos-config' hook
    
    * added .licrc
    
    * chmod +x hooks/backup-nixos.sh
    
    * added check-shebang-scripts-are-executable
    
    * changed shebang to /usr/bin/env bash
    
    * v0.1.2 (#5)
    
    * added 'backup-nixos-config' hook
    
    * added .licrc
    
    * chmod +x hooks/backup-nixos.sh
    
    * added check-shebang-scripts-are-executable
    
    * changed shebang to /usr/bin/env bash
    
    * explicitly set GIT_DIR w/ 'git rev-parse --show-toplevel'

--- hooks/backup-nixos.sh
+++ hooks/backup-nixos.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
+# If /etc/nixos exists, copy it to the git repo
 if [ -d "/etc/nixos" ]; then
   GIT_DIR=$(git rev-parse --show-toplevel)
   mkdir -p "$GIT_DIR/nixos/WIP"
-  cp -r /etc/nixos/ "$GIT_DIR/nixos/WIP/"
+  cp -r /etc/nixos/* "$GIT_DIR/nixos/WIP/"
   git add "$GIT_DIR/nixos/WIP"
 fi
