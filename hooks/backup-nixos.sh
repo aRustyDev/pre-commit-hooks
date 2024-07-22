e1bfd889a4177e508c6b0b4a8b1bf76dcc701caf
aRustyDev <36318507+aRustyDev@users.noreply.github.com>
2024-07-22 10:32:00 -0400

    Update backup-nixos.sh

--- hooks/backup-nixos.sh
+++ hooks/backup-nixos.sh
@@ -4,6 +4,6 @@
 if [ -d "/etc/nixos" ]; then
   GIT_DIR=$(git rev-parse --show-toplevel)
   mkdir -p "$GIT_DIR/nixos/WIP"
-  cp -a /etc/nixos/. "/$GIT_DIR/nixos/WIP/"
+  cp -a /etc/nixos/. "$GIT_DIR/nixos/WIP/"
   git add "$GIT_DIR/nixos/WIP"
 fi
