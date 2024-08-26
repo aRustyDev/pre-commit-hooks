8d2e1aa0bc028f4c77c4d99e4efe18a376b7dd22
aRustyDev <36318507+aRustyDev@users.noreply.github.com>
2024-08-26 09:32:31 -0400

    feat(v0.1.8): extending dead-links hook to check urls

--- hooks/docs/dead-links.sh
+++ hooks/docs/dead-links.sh
@@ -23,36 +23,72 @@ is_url() {
   fi
 }
 
+absolute_path() {
+  PREFIX_PATH="$1" # The path of the file
+  SUFFIX_PATH="$2" # The path of the link
+  # +++ Possible Path Formats Handled +++
+  # 1. ./file.md
+  # 2. ../file.md
+  # 3. /file.md
+  # 4. file.md
+  # Check if the link is an absolute path
+  if [[ "$SUFFIX_PATH" =~ ^/ ]]; then
+    echo "$SUFFIX_PATH"
+  # Check if the link is a relative path
+  elif [[ "$SUFFIX_PATH" =~ ^\./ ]]; then
+    echo "$(echo "$PREFIX_PATH" | rg '(.*)/.+$' -or '$1')/$SUFFIX_PATH"
+  # Check if the link is a relative path w/ parent directory hops
+  elif [[ "$SUFFIX_PATH" =~ ^\.\./ ]]; then
+    while [[ "$SUFFIX_PATH" =~ ^\.\./ ]]; do
+      PREFIX_PATH="$(echo "$PREFIX_PATH" | rg '(.*)/.+$' -or '$1')"
+      SUFFIX_PATH="$(echo "$SUFFIX_PATH" | rg '^\.\./(.+)$' -or '$1')"
+      shift
+    done
+    echo "$PREFIX_PATH/$SUFFIX_PATH" | tr '//' '/'
+  else
+    echo "$PREFIX_PATH/$SUFFIX_PATH" | tr '//' '/'
+  fi
+}
+
 # Loop through the files
-for file in $1; do
+for file in "$@"; do
   # Check if the file exists
   if [ -f "$file" ]; then
     # Get the links from the file
-    mapfile -t links < <(rg '\[[^\(]*\]\((\S+)\)' -or '$1' "$file")
+    IFS=" " read -r -a links <<< "$(rg '\[[^\(]*\]\((\S+)\)' -or '$1' "$file")"
     for link in "${links[@]}"; do
       # Split the link into its components
-      mapfile -t components < <(echo "$link" | tr '#' ' ')
+      IFS=" " read -r -a components <<< "$(echo "$link" | tr '#' ' ')"
       case "${#components[@]}" in
         # MEANS: No heading in the link
         1)
           # IF !URL && !FILE
           if is_url "${components[0]}"; then
             # Check if the link is reachable
-            if ! curl -s --head "${components[0]}" | head -n 1 | rg -cq '200'; then
+            if ! curl -s --head "${components[0]//\\/}" | head -n 1 | rg -cq '200'; then
               echo "$file: Link broken (URL not reachable): ${components[0]}"
               PASS=false
             fi
-          elif ! [ -f "${components[0]}" ]; then
-            echo "$file: Link broken (File not found): ${components[0]}"
+          # Build absolute path to the linked file to verify it exists
+          elif ! [ -f "$(absolute_path "$file" "${components[0]}")" ]; then
+            echo "$file: Link broken 1(File not found): ${components[0]}"
             PASS=false
           fi
           ;;
         # MEANS: The line contains a link w/ heading
         2)
-          # Check if the file the link points to exists
-          if ! [ -f "${components[0]}" ]; then
+          # IF !URL && !FILE
+          if is_url "${components[0]}"; then
+            # Check if the link is reachable
+            if ! curl -s --head "${components[0]//\\/}" | head -n 1 | rg -cq '200'; then
+              echo "$file: Link broken (URL not reachable): ${components[0]}"
+              PASS=false
+            fi
+          # Build absolute path to the linked file to verify it exists
+          elif ! [ -f "$(absolute_path "$file" "${components[0]}")" ]; then
             echo "$file: Link broken (File not found): ${components[0]}"
             PASS=false
+          # If Not a URL && File exists -> Check if the heading exists
           else
             # Check if the heading exists in the file
             SEARCH=$("^#+ $(echo "${components[1]}" | tr '-' ' ')")
@@ -68,7 +104,7 @@ for file in $1; do
                 ;;
               # MEANS: More than one heading found
               *)
-                echo "$file: More than one heading found: # ${components[1]}"
+                echo "$file: Error in Linked file (More than one heading found): # ${components[1]}"
                 PASS=false
                 ;;
             esac
